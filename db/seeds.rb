39913129301705b1895e30866e01f1b76edb7157
Britton Walker <bwalker1801@gmail.com>
2015-11-16 18:01:16 -0500

    full crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create!(title:'Wassup', body:'Let me tell you about a crazy little story. It all started when...')
