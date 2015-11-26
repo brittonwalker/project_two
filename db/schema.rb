ca8182bd9fd1020f60c217a256c81e1364fe203d
Britton Walker <bwalker1801@gmail.com>
2015-11-25 20:20:53 -0500

    remove some irrelevant code and beautify that ho

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121202500) do
+ActiveRecord::Schema.define(version: 20151125215824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,10 @@ ActiveRecord::Schema.define(version: 20151121202500) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.string   "img_url"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "img_url"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
